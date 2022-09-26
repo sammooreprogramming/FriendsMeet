524e3a35caa1902818a98b6fe81b84c293c3d46e
Sam Moore <sammooresemail23@gmail.com>
2022-09-26 11:18:39 -0700

    early server and all files structured apporpriately

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/FriendsMeet', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// for debugging purposes if needed.
+mongoose.set('debug', true);
+
+
+app.listen(PORT, () => console.log(`The server is live :${PORT}`));
